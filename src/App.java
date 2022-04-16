b5eb01178b90916851ab5aa4e5cd9e4dfb03a332
ggpsgeorge <ggpsgeorge@gmail.com>
2022-04-15 22:33:15 -0300

    Nounce incrementation n hash verification of block

--- src/App.java
+++ src/App.java
@@ -1,7 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Block block = new Block();
+        String hash = (Hashing.proof_of_work(block));
         System.out.println(block.toString());
-        System.out.println((Hashing.toHexString(Hashing.hash_256(block))));
+        System.out.println(hash);
+        System.out.println(Hashing.findHash(block));
     }
 }
--- src/Hashing.java
+++ src/Hashing.java
@@ -12,12 +12,38 @@ public class Hashing {
 
     public static String toHexString(byte[] hash){
         BigInteger number = new BigInteger(1, hash);
-
         StringBuilder hex = new StringBuilder(number.toString(16));
         
         while(hex.length() < 32){
             hex.insert(0, "0");
         }
         return hex.toString();
+    }
+
+    public static boolean isHashOk(String hash){
+        if(hash.substring(0, 3).equals("fff")){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static String proof_of_work(Block block) throws NoSuchAlgorithmException{
+        boolean found  = false;
+        String hash = "";
+
+        while(true){
+
+            hash = toHexString(hash_256(block));
+            found = isHashOk(hash);
+            if(found == true){break;}
+            block.setNounce(block.getNounce()+1);
+        }
+
+        return hash;
+    }
+
+    public static String findHash(Block block) throws NoSuchAlgorithmException{
+        return toHexString(hash_256(block));
     }
 }
